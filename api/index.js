7fde41d27b9323eec4f6f5d1c77ce786f1bf4230
zendi014 <48636084+zendi014@users.noreply.github.com>
2021-11-20 13:41:16 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
-const token = 'your_telegram_token';
+const token = '2133290397:AAHIgB1ZusJ-m2-TluhcnWMfZQNwfRPMk1U';
 const bot = new TelegramBot(token, {polling: true});
 
 
