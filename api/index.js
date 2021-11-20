283356312276388528821380765e0e42b4930755
zendi014 <48636084+zendi014@users.noreply.github.com>
2021-11-20 13:53:06 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -33,8 +33,12 @@ router.get('/', (req, res, next) => {
 });
 
 
-router.get('/sensor/:sensor1:sensor2', (req, res, next) => {
+router.get('/sensor/:sensor1/:sensor2', (req, res, next) => {
   try {
+      bot.sendMessage(
+            global_msg_id, //msg.id
+            `Pembacaan Sensor:: ${req.params.sensor1} ${req.params.sensor2}`
+     );
       res.json({
         "status": 202,
         "messgae": "Success",
