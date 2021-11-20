a002e24ed432f920e26d132062225940e0004d9a
zendi014 <48636084+zendi014@users.noreply.github.com>
2021-11-20 08:40:20 +0700

    update base
    update base

---
+++ api/index.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var router = express.Router();
+
+
+// Bot Setting
+const TelegramBot = require('node-telegram-bot-api');
+const token = 'your_telegram_token';
+const bot = new TelegramBot(token, {polling: true});
+
+
+let global_msg_id;
+// Main Menu Bot
+bot.onText(/\/start/, (msg) => {
+    global_msg_id = msg.chat.id;
+    bot.sendMessage(
+        global_msg_id,
+        `hello ${msg.chat.first_name}, welcome...\n
+        click /predict`
+    );
+});
+
+bot.on('message', (msg) => {
+  console.log(msg);
+});
+
+
+/* GET users listing. */
+router.get('/', (req, res, next) => {
+  res.json({
+    "status": 202,
+    "messgae": "Success"
+  });
+});
+
+
+router.get('/sensor/:sensor1:sensor2', (req, res, next) => {
+  try {
+      res.json({
+        "status": 202,
+        "messgae": "Success",
+        "data": {
+          "sensor_1": req.params.sensor1,
+          "sensor_2": req.params.sensor2
+        }
+      });
+  } catch (err) {
+      next(err);
+  }
+});
+
+
+r.get('/test/:key', function(req, res, next){
+    bot.sendMessage(
+            global_msg_id, //msg.id
+            `${req.params.key}`
+    );
+    res.json(req.params.key);
+});
+
+
+module.exports = router;
--- app.js
+++ app.js
@@ -5,22 +5,22 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var apiRouter = require('./api/index');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+   .engine('html', require('ejs').renderFile)
+   .set('view engine', 'ejs');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(logger('dev'))
+   .use(express.json())
+   .use(express.urlencoded({ extended: false }))
+   .use(cookieParser())
+   .use(express.static(path.join(__dirname, 'public')))
+   .use('/', indexRouter)
+   .use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
