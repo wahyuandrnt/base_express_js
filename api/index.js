0eecad40de3dfca656bf485575a34daf3820f16f
zendi014 <48636084+zendi014@users.noreply.github.com>
2021-11-20 14:06:09 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -32,7 +32,7 @@ router.get('/', (req, res, next) => {
   });
 });
 
-
+// https://esp-telebot.herokuapp.com/api/sensor/123/65/78
 router.get('/sensor/:sensor1/:sensor2/:sensor3', (req, res, next) => {
   try {
       bot.sendMessage(
@@ -53,7 +53,7 @@ router.get('/sensor/:sensor1/:sensor2/:sensor3', (req, res, next) => {
   }
 });
 
-
+// https://esp-telebot.herokuapp.com/api/test/cobacoba
 router.get('/test/:key', function(req, res, next){
     bot.sendMessage(
             global_msg_id, //msg.id
