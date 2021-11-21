e2c926d48ad6bce624a999b555d86209a86f73b9
wahyuandrnt <94786343+wahyuandrnt@users.noreply.github.com>
2021-11-21 22:18:29 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
-const token = '2133290397:AAHIgB1ZusJ-m2-TluhcnWMfZQNwfRPMk1U';
+const token = '2101780692:AAFEPQg_bsLDGDdjSBR3gtZo8KkYOWBOxoE';
 const bot = new TelegramBot(token, {polling: true});
 
 
